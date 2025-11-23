0755fdabd0afa2adca52cf2552d5955c83827a81
Petch Nieamchat <petch1235@gmail.com>
2025-11-23 19:08:38 +0700

    Create checklist_export_2025-11-23_13-05-47.sql

---
+++ MAC/exports/checklist_export_2025-11-23_13-05-47.sql
@@ -0,0 +1,23 @@
+-- Export สำหรับ พระสมุทรเจดีย์
+-- สร้างเมื่อ: 2025-11-23 13:05:47
+-- ระบบ: Employee Checklist System
+
+DROP TABLE IF EXISTS `พระสมุทรเจดีย์_backup`;
+CREATE TABLE `พระสมุทรเจดีย์_backup` LIKE `พระสมุทรเจดีย์`;
+
+INSERT INTO `พระสมุทรเจดีย์_backup` VALUES ('1', 'P1', 'น้ำดื่ม คริสตัล 350 มล.', 'เครื่องดื่ม', '../../image/products/water/คริสตัล350มล.jpg', 'out_of_stock', '1', '2025-11-23 19:05:47', NULL);
+INSERT INTO `พระสมุทรเจดีย์_backup` VALUES ('2', 'P2', 'น้ำดื่ม คริสตัล 600 มล.', 'เครื่องดื่ม', '../../image/products/water/คริสตัล600มล.jpg', 'in_stock', '1', '2025-11-23 19:05:47', NULL);
+INSERT INTO `พระสมุทรเจดีย์_backup` VALUES ('3', 'P3', 'น้ำดื่ม คริสตัล 1,000 มล.', 'เครื่องดื่ม', '../../image/products/water/คริสตัล1000มล.jpg', 'not_for_sale', '1', '2025-11-23 19:05:47', NULL);
+INSERT INTO `พระสมุทรเจดีย์_backup` VALUES ('4', 'P4', 'น้ำดื่ม คริสตัล 1,500 มล.', 'เครื่องดื่ม', '../../image/products/water/คริสตัล1500มล.jpg', 'in_stock', '1', '2025-11-23 19:05:47', NULL);
+INSERT INTO `พระสมุทรเจดีย์_backup` VALUES ('5', 'P5', 'น้ำดื่ม เนสท์เล่ 330 มล.', 'เครื่องดื่ม', '../../image/products/water/เนสท์เล่330มล.jpg', 'not_for_sale', '1', '2025-11-23 19:05:47', NULL);
+INSERT INTO `พระสมุทรเจดีย์_backup` VALUES ('6', 'P6', 'น้ำดื่ม เนสท์เล่ 600 มล.', 'เครื่องดื่ม', '../../image/products/water/เนสท์เล่600มล.jpg', 'in_stock', '1', '2025-11-23 19:05:47', NULL);
+INSERT INTO `พระสมุทรเจดีย์_backup` VALUES ('7', 'P7', 'น้ำดื่ม เนสท์เล่ 1,500 มล.', 'เครื่องดื่ม', '../../image/products/water/เนสท์เล่1500มล.jpg', 'not_for_sale', '1', '2025-11-23 19:05:47', NULL);
+INSERT INTO `พระสมุทรเจดีย์_backup` VALUES ('8', 'P8', 'น้ำดื่ม เนสท์เล่ 6,000 มล.', 'เครื่องดื่ม', '../../image/products/water/เนสท์เล่6000มล.jpg', 'not_for_sale', '1', '2025-11-23 19:05:47', NULL);
+INSERT INTO `พระสมุทรเจดีย์_backup` VALUES ('9', 'S1', 'คอนเน่', 'ขนม', '../../image/products/snack/คอนเน่.png', 'in_stock', '1', '2025-11-23 19:05:47', NULL);
+INSERT INTO `พระสมุทรเจดีย์_backup` VALUES ('10', 'S2', 'ซันไบทส์', 'ขนม', '../../image/products/snack/ซันไบทส์.png', 'out_of_stock', '1', '2025-11-23 19:05:47', NULL);
+INSERT INTO `พระสมุทรเจดีย์_backup` VALUES ('11', 'S3', 'โดริโทส', 'ขนม', '../../image/products/snack/โดริโทส.png', 'in_stock', '1', '2025-11-23 19:05:47', NULL);
+INSERT INTO `พระสมุทรเจดีย์_backup` VALUES ('12', 'S4', 'โตโร', 'ขนม', '../../image/products/snack/โตโร.png', 'in_stock', '1', '2025-11-23 19:05:47', NULL);
+INSERT INTO `พระสมุทรเจดีย์_backup` VALUES ('13', 'S5', 'เทสโต', 'ขนม', '../../image/products/snack/เทสโต.png', 'out_of_stock', '1', '2025-11-23 19:05:47', NULL);
+INSERT INTO `พระสมุทรเจดีย์_backup` VALUES ('14', 'S6', 'เลย์', 'ขนม', '../../image/products/snack/เลย์.png', 'in_stock', '1', '2025-11-23 19:05:47', NULL);
+INSERT INTO `พระสมุทรเจดีย์_backup` VALUES ('15', 'S7', 'โลตัส ขาไก่', 'ขนม', '../../image/products/snack/โลตัส ขาไก่.png', 'not_for_sale', '1', '2025-11-23 19:05:47', NULL);
+INSERT INTO `พระสมุทรเจดีย์_backup` VALUES ('16', 'S8', 'อาริงาโต', 'ขนม', '../../image/products/snack/อาริงาโต.png', 'not_for_sale', '1', '2025-11-23 19:05:47', NULL);
